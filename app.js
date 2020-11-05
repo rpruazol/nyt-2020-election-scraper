74e0ea27bf870aa750a15517e4d4c2d929dd8a26
Ray <rpruazol@gmail.com>
2020-11-05 14:34:14 -0800

    added filter

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 function search() {
     // get variables
     let input = document.getElementById('myInput');
-    let filter = input.value.toUpperCase();
+    let filter = input.value.toLowerCase();
     let tables = document.getElementsByTagName('table');
 
-    for(let i = 0; i < row.length; i++) {
+    for(let i = 0; i < tables.length; i++) {
         if (tables[i].id !== filter) {
             tables[i].style.display = 'none';
         };
